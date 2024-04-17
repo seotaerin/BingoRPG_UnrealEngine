71a9d0349d14617b8bc0993bf01a3c4daaee580a
seotaerin <taerin.j.s@gmail.com>
2024-04-17 21:50:48 +0900

    Source

---
+++ Source/BingoRPG.Target.cs
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BingoRPGTarget : TargetRules
+{
+	public BingoRPGTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "BingoRPG" } );
+	}
+}
